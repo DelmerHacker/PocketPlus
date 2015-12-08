64482bf2a839df2c4fe3c12bd211f6f0965f6dc7
xFlare <boughousp19@student.riverdell.org>
2015-12-08 10:56:35 -0500

    API!

---
+++ src/xFlare/xCore/API.php
@@ -0,0 +1,7 @@
+//TODO: LOGO
+
+/*
+- This class creates aa handy-dandy API to allow plugins to control these new features.
+- Each function only returns true or false.
+- This class is optional and only gets registered if enabled.
+*/
