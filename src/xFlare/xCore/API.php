a41f838b6b3a9b8d8bf8d393e5ca87aede1852c9
xFlare <boughousp19@student.riverdell.org>
2015-12-09 07:57:51 -0500

    First API key!

--- src/xFlare/xCore/API.php
+++ src/xFlare/xCore/API.php
@@ -1,3 +1,4 @@
+<?php
 /*
  _____           _        _   _____  _           
 |  __ \         | |      | | |  __ \| |          
@@ -12,3 +13,19 @@
 - Each function only returns true or false.
 - This class is optional and only gets registered if enabled.
 */
+namespace xFlare\PocketPlus;
+
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\Server;
+
+class API implements Listener{
+	   public function __construct(Loader $plugin){
+        $this->plugin = $plugin;
+    }
+    public function isWeatherToggle(){
+        return $this->plugin->weather;
+    }
+}
